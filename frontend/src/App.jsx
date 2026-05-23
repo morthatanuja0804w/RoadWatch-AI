0fe30e56067945b77f689a3dce5c5f9a6ed19329
tanuja0804 <tanujamortha@gmail.com>
2026-05-23 18:33:45 +0530

    Setup React Vite frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+        fontWeight: "bold",
+      }}
+    >
+      RoadWatch AI
+    </div>
+  );
+}
+
+export default App;
